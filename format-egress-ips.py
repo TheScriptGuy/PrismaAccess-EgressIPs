c81e071c0f7ec5e8470fd7a83689c80999b81b2a
Nolan <github@remotenode.org>
2022-04-11 15:47:14 -0700

    Initial upload

---
+++ format-egress-ips.py
@@ -0,0 +1,56 @@
+# Formats the json output to get all the egress IPs
+
+import sys
+import json
+import argparse
+from os import path
+
+scriptVersion = "0.01"
+
+def parseArguments():
+    """
+    Create argument options and parse through them to determine what to do with script.
+    """
+
+    # Instantiate the parser
+    global scriptVersion
+    parser = argparse.ArgumentParser(description='Format Egress IPs ' + scriptVersion)
+
+    # Optional arguments
+    parser.add_argument('--fileName', default="egress-ips.json",
+                        help='List of json formatted egress IPs')
+    
+    global args    
+    args = parser.parse_args()
+
+def main():
+    parseArguments()
+    if args.fileName:
+        if not path.exists(args.fileName):
+            print('I cannot find file ' + args.fileName)
+            sys.exit(1)
+        else:
+            f = open(args.fileName)
+
+            egressIps = json.load(f)
+
+            # Print Headers
+            print('{: <20}\t{: <18}\t{: <18}'.format("Location", "serviceType", "egress IP"))
+            for objEgressIps in egressIps["result"]:
+                for obj in objEgressIps["address_details"]:
+                    print('{: <20}\t{: <18}\t{: <18}'.format(objEgressIps["zone"], obj["serviceType"], obj["address"] ))
+
+
+if __name__ == '__main__':
+    try:
+        
+        main()
+
+    except KeyboardInterrupt:
+        print('Interrupted')
+        print
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
